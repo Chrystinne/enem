9a4890e85a264f4f14f91966b9980f7003f43f57
Diénert Vieira <dienertalencar@gmail.com>
2022-03-21 13:49:39 -0300

    Changing graphs

--- src/dashboard.py
+++ src/dashboard.py
@@ -10,65 +10,119 @@ import plotly.graph_objs as go
 
 import leafmap.foliumap as leafmap
 
-# from utils import load_parquets
-
-
+from tempo import inicio, fim
 
 # can only set this once, first thing to set
 st.set_page_config(layout="wide")
 
 titles_and_graphs = {
-    "1 - Geographical belongingness": {"type": 'geo', "questions": ""},
-    "2 - Age": {"type": None, "questions": ""},
-    "3 - Gender": {"type": "pyramid", "questions": ""},
-    "4 - Marital status": {"type": None, "questions": ""},
-    "5 - Ethnicity": {"type": None, "questions": ""},
-    "6 - Parents's education level": {"type": "parallel", "questions": ["Q001", "Q002"]},
-    "7 - Parents's profession": {"type": "parallel", "questions": ["Q003", "Q004"]},
-    "8 - Income": {"type": 'cloro', "questions": ""},
-    "9 - Socioeconomic Status": {"type": None, "questions": ""},
+    "Geographical belongingness": {"type": 'geo', "questions": ""},
+    "Age": {"type": None, "questions": "", "dimension": "age"},
+    "Gender": {"type": "pyramid", "questions": ""},
+    "Parents' education level": {"type": "pyramid", "questions": ["Q001", "Q002"]},
+    "Marital status": {"type": None, "questions": ""},
+    "Ethnicity": {"type": None, "questions": ""},
+    "Father's education level": {"type": "parallel", "questions": ["Q001"]},
+    "Mother's education level": {"type": "parallel", "questions": ["Q002"]},
+    "Father's profession": {"type": "parallel", "questions": ["Q003"]},
+    "Mother's profession": {"type": "parallel", "questions": ["Q004"]},
+    "Income": {"type": 'cloro', "questions": ""},
+    "Socioeconomic Status": {"type": None, "questions": ""},
 }
 
 factors = titles_and_graphs.keys()
-
-# Get data
-@st.cache()
-def load_data():
-
-    path = '../datasets/integrated_datas.parquet.gzip/part.0.parquet'
-
-    # return pd.read_parquet(path)
-
-    return dd.read_parquet('../datasets/integrated_datas.parquet.gzip', ignore_metadata_file=True)
-
-
-ddf_original = load_data().copy(deep=False)
 
 # Top text area
 with st.container():
     st.title("Education Manager's Guide 📊")
 
-left_column, right_column = st.columns(2)
-with left_column:
+# column_1, column_2, column_3 = st.columns(3)
+(column_1, column_2), test_data = st.columns(2), False
+with column_1:
     chart_type = st.selectbox("Choose the factor you would like to analyse", factors, 2)
-with left_column:
-    years = ddf_original.NU_ANO.astype(int).unique().compute()
+with column_2:
+    years = list(range(2015,2021))
     year = st.selectbox("Year", years, len(years)-1)
+# with column_3:
+#     test_data = st.checkbox('Test Data', True)
+# with column_3:
+#     test_data = st.checkbox('Test Data', True)
+test = '_test' if test_data else ''
 
-ddf = ddf_original[ddf_original['NU_ANO'] == year]
+print(f"Year: {year}")
+print(f"Test data: {test_data}")
 
-def our_plot(params, ddf, st):
+# Get 2015 data
+@st.cache()
+def load_2015_data():
+
+    path = f'../datasets/integrated_datas_2015{test}.parquet.gzip'
+    return dd.read_parquet(path, ignore_metadata_file=True)#, columns=cols_used)
+
+# Get 2016 data
+@st.cache()
+def load_2016_data():
+
+    path = f'../datasets/integrated_datas_2016{test}.parquet.gzip'
+    return dd.read_parquet(path, ignore_metadata_file=True)#, columns=cols_used)
+
+# Get 2017 data
+@st.cache()
+def load_2017_data():
+
+    path = f'../datasets/integrated_datas_2017{test}.parquet.gzip'
+    return dd.read_parquet(path, ignore_metadata_file=True)#, columns=cols_used)
+
+# Get 2018 data
+@st.cache()
+def load_2018_data():
+
+    path = f'../datasets/integrated_datas_2018{test}.parquet.gzip'
+    return dd.read_parquet(path, ignore_metadata_file=True)#, columns=cols_used)
+
+# Get 2019 data
+@st.cache()
+def load_2019_data():
+
+    path = f'../datasets/integrated_datas_2019{test}.parquet.gzip'
+    return dd.read_parquet(path, ignore_metadata_file=True)#, columns=cols_used)
+
+# Get 2020 data
+@st.cache()
+def load_2020_data():
+
+    path = f'../datasets/integrated_datas_2020{test}.parquet.gzip'
+    return dd.read_parquet(path, ignore_metadata_file=True)#, columns=cols_used)
+
+
+if year == 2020:
+    ddf = load_2020_data()
+elif year == 2019:
+    ddf = load_2019_data()
+elif year == 2018:
+    ddf = load_2018_data()
+elif year == 2017:
+    ddf = load_2017_data()
+elif year == 2016:
+    ddf = load_2016_data()
+elif year == 2015:
+    ddf = load_2015_data()
+else:
+    ddf = load_2020_data()
+
+def our_plot(params, ddf_par, st):
     """ return plotly plots """
 
+    init = inicio()
     if params["type"] == "geo":
         st.title('Geographical belongingness')
 
 
-        ddf = pd.read_csv("../datasets/heatmap.csv")
+        ddf_par = pd.read_csv("../datasets/heatmap.csv")
 
         m = leafmap.Map(center=[-16, -50], tiles="stamentoner", zoom_start=5)
         m.add_heatmap(
-            ddf,
+            ddf_par,
             "latitude",
             "longitude",
             "Q006",
@@ -84,11 +138,11 @@ def our_plot(params, ddf, st):
         st.title('Geographical belongingness')
 
 
-        ddf = pd.read_csv("../datasets/heatmap.csv")
+        ddf_par = pd.read_csv("../datasets/heatmap.csv")
 
         m = leafmap.Map(center=[-16, -50], tiles="stamentoner", zoom_start=5)
         m.add_heatmap(
-            ddf,
+            ddf_par,
             "latitude",
             "longitude",
             "Q006",
@@ -109,42 +163,92 @@ def our_plot(params, ddf, st):
 
     elif params["type"] == "pyramid":
 
-        filtro = ddf.groupby(['SG_UF_RESIDENCIA', 'TP_SEXO', 'NU_ANO'])['NU_NOTA_MT'].mean().sort_index(ascending=False)#.compute()
-        women = filtro[filtro.index.get_level_values('TP_SEXO').isin(['F'])]
-        men = filtro[filtro.index.get_level_values('TP_SEXO').isin(['M'])]
-        estados = men.index.get_level_values(0)
-        men_bins = np.array(men)
-        women_bins = np.array(women)
-        women_bins *= -1
-        y = estados
 
-        layout = go.Layout(yaxis=go.layout.YAxis(title='Mathematics Grades per State'),
-                        xaxis=go.layout.XAxis(
-                            range=[-1000, 1000],
-                            tickvals=[-1000, -700, -300, 0, 300, 700, 1000],
-                            ticktext=[1000, 700, 300, 0, 300, 700, 1000],
-                            title='Mean Grade'),
-                        barmode='overlay',
-                        bargap=0.1, width=50, height=800)
+        if 'questions' in params:
+            q1 = params['questions'][1]
+            q2 = params['questions'][0]
+            filtro1 = ddf_par.groupby([q1, 'NU_ANO'])['NU_NOTA_LC'].mean().compute().sort_index(ascending=False)
+            filtro2 = ddf_par.groupby([q2, 'NU_ANO'])['NU_NOTA_LC'].mean().compute().sort_index(ascending=False)
+            women = filtro1.index.get_level_values(q1)
+            men = filtro2.index.get_level_values(q2)
+            women_bins = np.array(filtro1.values)
+            men_bins = np.array(filtro2.values)
+            women_bins *= -1
+            y = men
 
-        data_ = [go.Bar(y=y,
-                    x=men_bins,
-                    orientation='h',
-                    name='Men',
-                    hoverinfo='y',
-                    text=men_bins.astype('int'),
-                    marker=dict(color='purple')
-                    ),
-                go.Bar(y=y,
-                    x=women_bins,
-                    orientation='h',
-                    name='Women',
-                    text=-1 * women_bins.astype('int'),
-                    hoverinfo='y',
-                    marker=dict(color='seagreen')
-                    )]
+            print(men_bins)
+            print(women_bins)
 
-        st.plotly_chart({"data":data_, "layout":layout}, use_container_width=True)
+            layout = go.Layout(yaxis=go.layout.YAxis(title='Languages and Codes Grades per Parents\' Education Level',
+                                                    #  tickformat='.2f'
+                                                     ),
+                            xaxis=go.layout.XAxis(
+                                range=[-700, 700],
+                                tickvals=[-700, -350, 0, 350, 700],
+                                ticktext=[700, 350, 0, 350, 700],
+                                title='Mean Grade',
+                                # tickformat='.2f'
+                                ),
+                            barmode='overlay',
+                            bargap=0.1, width=50, height=800)
+
+            data_ = [go.Bar(y=y,
+                        x=men_bins,
+                        orientation='h',
+                        name='Men',
+                        hoverinfo='y',
+                        text=men_bins,
+                        marker=dict(color='purple')
+                        ),
+                    go.Bar(y=y,
+                        x=women_bins,
+                        orientation='h',
+                        name='Women',
+                        text= -1 * women_bins,
+                        hoverinfo='y',
+                        marker=dict(color='seagreen')
+                        )]
+            fig = go.Figure(data=data_, layout=layout)
+            fig.update_traces(texttemplate="%{x:.2f}")
+
+        else:
+
+            filtro = ddf_par.groupby(['SG_UF_RESIDENCIA', 'TP_SEXO', 'NU_ANO'])['NU_NOTA_MT'].mean().compute().sort_index(ascending=False)
+            women = filtro[filtro.index.get_level_values('TP_SEXO').isin(['F'])]
+            men = filtro[filtro.index.get_level_values('TP_SEXO').isin(['M'])]
+            estados = men.index.get_level_values(0)
+            men_bins = np.array(men)
+            women_bins = np.array(women)
+            women_bins *= -1
+            y = estados
+
+            layout = go.Layout(yaxis=go.layout.YAxis(title='Mathematics Grades per State'),
+                            xaxis=go.layout.XAxis(
+                                range=[-1000, 1000],
+                                tickvals=[-1000, -700, -300, 0, 300, 700, 1000],
+                                ticktext=[1000, 700, 300, 0, 300, 700, 1000],
+                                title='Mean Grade'),
+                            barmode='overlay',
+                            bargap=0.1, width=50, height=800)
+
+            data_ = [go.Bar(y=y,
+                        x=men_bins,
+                        orientation='h',
+                        name='Men',
+                        hoverinfo='y',
+                        text=men_bins.astype('int'),
+                        marker=dict(color='purple')
+                        ),
+                    go.Bar(y=y,
+                        x=women_bins,
+                        orientation='h',
+                        name='Women',
+                        text=-1 * women_bins.astype('int'),
+                        hoverinfo='y',
+                        marker=dict(color='seagreen')
+                        )]
+
+        st.plotly_chart(fig, use_container_width=True)
         # display data
         # with st.container():
         #     show_data = st.checkbox("See the raw data?")
@@ -154,10 +258,18 @@ def our_plot(params, ddf, st):
 
     elif params["type"] == "parallel":
         print(params)
-        question = params["question"]
-        print(question)
-        df_par = ddf[[question, 'NU_NOTA_CN', 'NU_NOTA_CH', 'NU_NOTA_LC', 'NU_NOTA_MT', 'NU_NOTA_REDACAO']].compute()
-        # campos das notas da redação
+        columns = params["questions"]
+        questions = params["questions"].copy()
+        columns.extend(['NU_NOTA_CN', 'NU_NOTA_CH', 'NU_NOTA_LC', 'NU_NOTA_MT', 'NU_NOTA_REDACAO', 'NU_NOTA_SUM', 'NU_ANO'])
+        print(f'Colunas: {columns}')
+        # ddf_par =  dd.read_parquet(f'../datasets/integrated_datas_{year}{test}.parquet.gzip', ignore_metadata_file=True, columns=columns)
+        # ddf_par =  pd.read_parquet(f'../datasets/integrated_top_grade_data{test}.parquet.gzip', columns=columns)
+        ddf_par =  pd.read_parquet(f'../datasets/integrated_10000_top_grade_data.parquet.gzip', columns=columns)
+        ddf_par = ddf_par[ddf_par.NU_ANO == year]
+        print(f"Registros: {len(ddf_par)}")
+
+        # ddf_par = ddf_par.compute()
+        # campos das notas da redação   
         # df_par = ddf[[question, 'NU_NOTA_COMP1', 'NU_NOTA_COMP2', 'NU_NOTA_COMP3', 'NU_NOTA_COMP4', 'NU_NOTA_COMP5']].copy(deep=True).iloc[:1000]
 
         dict_names_exams = {'NU_NOTA_CN' : 'Ciências da Natureza',
@@ -165,6 +277,7 @@ def our_plot(params, ddf, st):
                             'NU_NOTA_LC' : 'Linguagens e Códigos',
                             'NU_NOTA_MT' : 'Matemática',
                             'NU_NOTA_REDACAO' : 'Redação',
+                            'NU_NOTA_SUM' : 'Soma das Notas',
 
                             'NU_NOTA_COMP1': 'Formal Writting', 
                             'NU_NOTA_COMP2': 'Topic Understanding', 
@@ -172,7 +285,11 @@ def our_plot(params, ddf, st):
                             'NU_NOTA_COMP4': 'Linguistic Mechanisms', 
                             'NU_NOTA_COMP5': 'Respect for human rights',
 
+                            'Q001': "Fahter's Education Level",
+                            'Q002': "Mother's Education Level",
+
                             'Q003': "Fahter's Profession",
+                            'Q004': "Mother's Profession",
                             
                             }
 
@@ -183,15 +300,13 @@ def our_plot(params, ddf, st):
                                         'E' : 'Grupo 5: Médico, engenheiro, dentista, psicólogo, economista, advogado, juiz, promotor, defensor, delegado, tenente, capitão, coronel, professor universitário, diretor em empresas públicas ou privadas, político, proprietário de empresas com mais de 10 empregados.',
                                         'F' : 'Não sei.'}}
 
-        if question == 'Q003':
-            question = "Fahter's Profession" 
-            dict_names_exams['Q003'] = question 
-        if question == 'Q004':
-            question = "Mother's Profession" 
-            dict_names_exams['Q004'] = question 
-        df_par = df_par.rename(columns=dict_names_exams)         
-        df_par[question] = df_par[question].map({"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7})
-        cmax = df_par[question].max()
+        for question in questions:     
+            print(question)
+            ddf_par[question] = ddf_par[question].map({"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7})
+        ddf_par = ddf_par.rename(columns=dict_names_exams)    
+        # print(ddf_par[['Ciências da Natureza', 'Ciências Humanas', 'Linguagens e Códigos', 'Matemática']])
+        question = dict_names_exams[questions[0]]
+        cmax = ddf_par[question].max()#.compute()
         # n_answers = df_par.to_dask_array(lengths=True)[question].nunique().compute()
         # group_vals = list(range(1,n_answers))
         # group_names = [f'Group {num}' for num in range(1,n_answers)]
@@ -199,9 +314,17 @@ def our_plot(params, ddf, st):
         # print(f"Columns: {df_par.columns}")
         # print(f"Question: {question}")
         # print(f"Groups: {group_names}")
-        for column in df_par.columns:
-            df_column = df_par[column]#.compute() 
-            item = dict(range = [df_column.min(), df_column.max()],
+        ddf_par.drop(columns=['NU_ANO'], inplace=True)
+        for column in ddf_par.columns:
+            df_column = ddf_par[column]#.compute()
+            min, max = df_column.min(), df_column.max()
+            if column == 'NU_ANO':
+                min = 2015
+                max = 2020
+            print(f"Column: {column}")
+            print(f"min: {min}")
+            print(f"max: {max}")
+            item = dict(range = [min, max],
                         label = column, 
                         values = list(df_column.values))
             # if column == question:
@@ -213,8 +336,7 @@ def our_plot(params, ddf, st):
         
         fig = go.Figure(data=
             go.Parcoords(
-                # line_color='yellow',
-                line = dict(color = df_par[question],
+                line = dict(color = ddf_par[question],
                         colorscale = [[0,'green'],[0.25,'blue'],[0.5,'red'],[1,'gold']],
                         showscale = True,
                         cmin = 0,   
@@ -230,21 +352,8 @@ def our_plot(params, ddf, st):
         #     if show_data:
         #         df_par
 
-num_questions = len(titles_and_graphs[chart_type]["questions"])
+    duration = fim(init)
+    print(f"Duração: {duration}\n")
 
-if num_questions > 0:
-    cols = st.columns(num_questions)
-else:
-    with st.container():
-        plot = our_plot(titles_and_graphs[chart_type], ddf, st)
-
-for index, question in enumerate(titles_and_graphs[chart_type]["questions"]):
-    titles_and_graphs[chart_type]['question'] = question
-    print(titles_and_graphs[chart_type])
-    with cols[index]:
-        if index == 0:
-            st.subheader(f"Father")
-        else:
-            st.subheader(f"Mother")
-        plot = our_plot(titles_and_graphs[chart_type], ddf, st)
-
+with st.container():
+    plot = our_plot(titles_and_graphs[chart_type], ddf, st)
